6d9f737a6b46218fbbce9f9eb706746c826e7f96
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-14 21:08:41 +0100

    Update

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,2 +1,10 @@
 import { useState, useEffect } from react;
 import './styles/weatherStyles.css'
+
+
+
+
+
+    let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+    let api_key = 'ab466f6ec21f347319f31f3787f5b3f'
+    let difKelvin = 273.15

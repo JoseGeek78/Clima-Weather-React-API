35acd13c47edeb3380e11f42a4f09d049b9fe9c7
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-14 06:26:37 +0100

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import PrimerComponente from './assets/PrimerComponente';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <PrimerComponente />
   </React.StrictMode>,
 )

6f99a7fa385847088160b02310c00c41c18e8e35
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-14 21:01:56 +0100

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { PrimerComponente } from './assets/PrimerComponente';
+import { WeatherApp } from './WeatherApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PrimerComponente />
+    <WeatherApp />
   </React.StrictMode>,
 )

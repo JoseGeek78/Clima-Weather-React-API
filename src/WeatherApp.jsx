0b2a91378bacf56691f5b0c3d3c3dea37ca4f5c0
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-15 05:37:05 +0100

    Update

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,12 +1,39 @@
+import { useState } from 'react'
 export const WeatherApp = () => {
-  return (
-    <div className="container">
+
+
+const [ciudad, setCiudad] = useState('')
+const [dataClima, setdataClima] = useState(null)
+  const handleCambioCiudad = (e) => {
+   setCiudad(e.target.value)
+}
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+      if(ciudad.length > 0) fetchClima()
+  }
+
+
+   return (
+
+      <div className="container">
 
       <h1>Aplicación del clima</h1>
 
+     <form onSubmit={handleSubmit}>
+       <input type="text" 
+       value={ciudad}
+       onChange={handleCambioCiudad}
+       />
+       <button type='submit'>Buscar</button>
+      </form>
+
+    
     </div>
   )
 }
+
+export default WeatherApp
 
 
 

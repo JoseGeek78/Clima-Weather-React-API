9af547eae3b2bbc00b8df159f2aa5b02f1f2b65c
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-15 06:34:46 +0100

    Finished project

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,56 +1,58 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-export const WeatherApp = () => {
+export const WheatherApp = () => {
 
-  const urlBase = 'https://api.openweathermap.org/data/3.0/onecall'
-  
+    const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+    const API_KEY = 'INSERT_YOUR_API_KEY'
+    const difKelvin = 273.15
 
+    const [ciudad, setCiudad] = useState('')
+    const [dataClima, setDataClima] = useState(null)
 
-const [ciudad, setCiudad] = useState('')
-const [dataClima, setDataClima] = useState(null)
-  const handleCambioCiudad = (e) => {
-   setCiudad(e.target.value)
-}
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-      if(ciudad.length > 0) fetchClima()
-  }
-
-  const fetchClima = async () => {
-    try{
-      
-      const response = await fetch(`${urlBase}?lat=${40.4568}&lon=${-3.4796}&appid=${b4db614ba49fa5f7573dd1a0c0eb3ce8}`)
-      const data = await response.json()
-      setDataClima(data)
-    }catch(error){
-      console.error('Ocurrió el siguiente problema: ', error)
+    const handleCambioCiudad = (e) => {
+        setCiudad(e.target.value)
     }
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(ciudad.length > 0) fetchClima()
+    }
 
-   return (
+    const fetchClima = async () => {
+        try{
+            const response = await fetch(`${urlBase}?q=${ciudad}&appid=${API_KEY}`)
+            const data = await response.json()
+            setDataClima(data)
+        }catch(error){
+            console.error('Ocurrió el siguiente problema: ', error)
+        }
+    }
 
-      <div className="container">
 
-      <h1>Aplicación del clima</h1>
+  return (
 
-     <form onSubmit={handleSubmit}>
-       <input type="text" 
-       value={ciudad}
-       onChange={handleCambioCiudad}
-       />
-       <button type='submit'>Buscar</button>
-      </form>
-
-    
+    <div className="container">
+        <h1>Aplicación del Clima</h1>
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="text"
+            value={ciudad}
+            onChange={handleCambioCiudad}
+            />
+            <button type="submit">Buscar</button>
+        </form>  
+        {
+            dataClima && (
+                <div>
+                    <h2>{dataClima.name}</h2>
+                    <p>Temperatura: {parseInt(dataClima?.main?.temp - difKelvin)}ºC</p>
+                    <p>Condición meteorológica: {dataClima.weather[0].description}</p>
+                    <img src={`https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`}/>
+                </div>
+            )
+        }
     </div>
   )
 }
-
-export default WeatherApp
-
-
-
 
 
     
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { WeatherApp } from './WeatherApp.jsx'
+import { WheatherApp } from './WheatherApp'
 import './styles/weatherStyles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WeatherApp />
+    <WheatherApp />
   </React.StrictMode>,
 )
